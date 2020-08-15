c48ac58ff77495a9e96274d8e9dfaf0702ee1b1d
shanart <shanin.x@gmail.com>
2020-08-15 23:51:36 +0300

    update passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,22 @@
-import os
-import sys
+import myapp.wsgi
+SCRIPT_NAME = '/home/shankvza/personal'
 
+class PassengerPathInfoFix(object):
+    """
+    Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
+    """
+    def __init__(self, app):
+        self.app = app
 
-sys.path.insert(0, os.path.dirname(__file__))
+    def __call__(self, environ, start_response):
+        from urllib.parse import unquote
+        environ['SCRIPT_NAME'] = SCRIPT_NAME
 
+        request_uri = unquote(environ['REQUEST_URI'])
+        script_name = unquote(environ.get('SCRIPT_NAME', ''))
+        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+        return self.app(environ, start_response)
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+application = myapp.wsgi.application
+application = PassengerPathInfoFix(application)
