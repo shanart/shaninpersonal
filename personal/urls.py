ecd26fdaa912ef9843e52bf0d8498469be1ac3fb
shanart <shanin.x@gmail.com>
2020-08-16 00:24:15 +0300

    update templates

--- personal/urls.py
+++ personal/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="base.html"), name="home"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
